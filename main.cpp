181d19d71162ce21a2f861bb31bf8aec4b2576a5
Lack54 <timoraikkon@gmail.com>
2026-07-22 11:47:04 -0400

    Added print out for base vectors test

--- main.cpp
+++ main.cpp
@@ -5,27 +5,30 @@
 #include "orbitalElements.h"
 #include "structs.h"
 #include "utils.h"
-#include <numbers>
+
 int main(){
   const CelestialBody& body = Earth;
-
-  vec3 positionVector {230, 500, 4567};
-  vec3 velcoityVector {440, 500, 503}; 
-  vec3 angularMomentumVector {angularMomentumVector_calc(positionVector, velcoityVector)};
+  vec3 positionVector {0, 500, 0};
+  vec3 velcoityVector {0, 500, 500}; 
+  
     
   double r1 {88000000 + body.radius }, r2 {7000000 + body.radius};
   double a {semiMajorAxis_calc(r1, r2, body)};
   double e {eccentricity_calc(r1,r2)};
   double b {semiMinorAxis_calc(a,e)};
   double nu { trueAnomaly_calc(e,a,55000000)};
-  double inclination {inclination_calc(angularMomentumVector)};
+  vec3 angularMomentumVector {angularMomentumVector_calc(positionVector, velcoityVector)};
 
 
   std::cout << "position vector";
+  printVector(positionVector);
+  std::cout << "Velocity Vector";
+  printVector(velcoityVector);
+  std::cout << "angular Momentum Vector";
   printVector(angularMomentumVector);
+  
+  std::cout << "Inclination calculated: " << inclination_calc(angularMomentumVector) << '\n';
 
-  std::cout << "Inclination calculated: " << inclination << '\n';
-  std::cout << std::numbers::pi; 
 //  std::cout << e <<'\n'<< a << '\n';
 
  // double dV = circularizeOrbit_Calc(a, r1, r2, body);
